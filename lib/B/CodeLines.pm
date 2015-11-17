9a8355e669c4effb0c23e6c33476a2ff62f05380
Rocky Bernstein <rocky@cpan.org>
2015-11-17 18:16:25 -0500

    WIP - towards getting line counts as well as line numbers

--- lib/B/CodeLines.pm
+++ lib/B/CodeLines.pm
@@ -1,11 +1,11 @@
 package B::CodeLines;
-# Copyright (C) 2012 Rocky Bernstein. All rights reserved.
+# Copyright (C) 2012, 2015 Rocky Bernstein. All rights reserved.
 # This program is free software; you can redistribute and/or modify it
 # under the same terms as Perl itself.
 
-use strict; use warnings; 
+use strict; use warnings;
 
-our $VERSION   = '1.1';
+our $VERSION   = '1.2';
 
 use B qw(class);
 use B::Utils;
@@ -18,23 +18,23 @@ use vars qw(@results);
 sub gather($) {
     my $top_file = shift;
 
-    my $callback = sub { 
+    my $callback = sub {
         my $op = $_[0];
-        $op->codelines($_[1]) 
+        $op->codelines($_[1])
             if 'COP' eq B::class($op) and $top_file eq $op->file;
     };
 
     # Enbugger->stop;
 
-    foreach (values B::Utils::all_roots) {
+    foreach (values %{B::Utils::all_roots()}) {
         eval { B::Utils::walkoptree_simple( $_, $callback ); }
     }
 
-    foreach (values B::Utils::all_starts) {
+    foreach (values %{B::Utils::all_starts()}) {
         eval { B::Utils::walkoptree_simple( $_, $callback ); }
     }
 
-    foreach (values B::Utils::anon_subs) {
+    foreach (@{B::Utils::anon_subs()}) {
         eval { B::Utils::walkoptree_simple( $_, $callback ); }
     }
 
@@ -43,14 +43,41 @@ sub gather($) {
 
 }
 
-sub main() {
+sub gather_counts($)
+{
+    my $results = shift;
+    my %counts = ();
+    foreach my $tuple (@{$results}) {
+	$counts{$tuple->[0]}++;
+    }
+    return \%counts;
+}
+
+sub main($) {
+    my $opts = shift;
     my ($pkg, $top_file, $rest) = caller(2);
     gather($top_file);
-    foreach my $tuple (@results) { print "$tuple->[0]\n"; }
+    if ($opts->{show_count}) {
+	my $counts = gather_counts(\@results);
+	foreach my $key (sort {$a <=> $b} keys(%{$counts})) {
+	    print "$key $counts->{$key}\n";
+	}
+    } else  {
+	foreach my $tuple (@results) { print "$tuple->[0]\n"; }
+    }
 }
 
 sub compile {
-    return \&main
+    my $opts = {
+	show_count => 0,
+    };
+    while (my $arg = shift @_) {
+	if ($arg eq "-c") {
+	    $opts->{show_count} = 1;
+	}
+    }
+
+    main($opts);
 }
 
 sub B::OP::codelines {
@@ -63,9 +90,17 @@ sub B::OP::codelines {
 
 # Demo code
 unless (caller) {
-    gather(__FILE__);
-    foreach my $tuple (@results) { 
-	printf "%02d %0x\n", $tuple->[0], $tuple->[1]; 
+    # A line like the one below with more than one statement should
+    # appear more than once;
+    my $file = __FILE__; my $tuple;
+    gather($file);
+    foreach $tuple (@results) {
+	printf "%02d 0x%x\n", $tuple->[0], $tuple->[1];
+    }
+    print '-' x 20, "\n";
+    my $counts = gather_counts(\@results);
+    foreach my $key (sort {$a <=> $b} keys(%{$counts})) {
+	print "$key $counts->{$key}\n";
     }
 };
 
